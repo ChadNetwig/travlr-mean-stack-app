d1a335856291b2ffb547428997a51938a2d92092
Chad Netwig <chadnetwig@gmail.com>
2022-05-19 21:21:41 -0700

    Converted travel HBS template from static HTML to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
     // pageTitle = process.env.TRAVEL_TITLE + ' - Travel';
     // CLN: did not use because requires environment variable to be set
     //res.render('travel', { title: pageTitle });
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips });
    };
 module.exports = {
     travel
